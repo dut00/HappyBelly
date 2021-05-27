2b73a9290d10276fc51ed503f33c9da6a7b4c0ea
Kuba <kuba.dutk@gmail.com>
2021-05-27 23:12:44 +0200

    coordinates struct added

---
+++ HappyBellyApi/Model/Coordinates.cs
@@ -0,0 +1,16 @@
+﻿
+namespace HappyBellyApi.Models
+{
+    public struct Coordinates
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public Coordinates(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+        public override string ToString() => $"({Latitude}, {Longitude})";
+    }
+}
